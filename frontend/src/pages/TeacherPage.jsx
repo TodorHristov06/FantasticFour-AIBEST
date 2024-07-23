63af95069c5ec8a38a5b3ecc82fdb4c657c78592
Тодор Христов <todorhristov2006@gmail.com>
2024-07-23 20:50:50 +0300

    Update TeacherPage

--- frontend/src/pages/TeacherPage.jsx
+++ frontend/src/pages/TeacherPage.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import Sidebar from "../components/Sidebar";
-import TeacherContent from "../components/TeacherContent"; // Създайте този компонент
-import Profile from "../components/Profile";
+import TeacherContent from "../components/TeacherContent";
 import "../styles/teacherPage.css";
 
 const TeacherPage = () => {
   const userRole = 'teacher';
+  
   return (
     <div className="dashboard dashboard-green">
-        <Sidebar role={userRole} />
+      <Sidebar role={userRole} />
       <div className="dashboard--content">
         <TeacherContent />
-        {/* <Profile role={userRole} /> */}
       </div>
     </div>
   );
