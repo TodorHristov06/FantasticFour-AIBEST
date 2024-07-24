6abd99496716e674e598a71eeca68b8058061fa5
Тодор Христов <todorhristov2006@gmail.com>
2024-07-24 17:05:50 +0300

    Update teacher list

--- frontend/src/components/TeacherList.jsx
+++ frontend/src/components/TeacherList.jsx
@@ -2,63 +2,43 @@ import React from 'react';
 import "../styles/teacherList.css";
 import ProfilePicture from '../assets/ProfilePicture.jpg';
 
-// Добавете уникален идентификатор на всеки обект
-const teachers = [
-    {
-        id: 1,  // Уникален идентификатор
-        image: ProfilePicture,
-        name: "Stanimir Stoleotv",
-        duration: '2 hours lesson',
-        cost: '1',
-    },
-    {
-        id: 2,  // Уникален идентификатор
-        image: ProfilePicture,
-        name: "Georgi Dimitrov",
-        duration: '1 hour lesson',
-        cost: '2',
-    },
-    {
-        id: 3,  // Уникален идентификатор
-        image: ProfilePicture,
-        name: "Petar Petrov",
-        duration: '3 hours lesson',
-        cost: '1',
-    },
-    {
-        id: 4,  // Уникален идентификатор
-        image: ProfilePicture,
-        name: "Sheytanova",
-        duration: '4 hours lesson',
-        cost: '3',
-    },
-];
+// Примерни данни за текущия учител
+const currentTeacher = {
+    id: 1,
+    image: ProfilePicture,
+    name: "Maria Ivanova",
+    classes: [
+        { className: 'Mathematics - 5th Grade', time: 'Monday & Wednesday: 10 AM - 2 PM' },
+        { className: 'Mathematics - 6th Grade', time: 'Friday: 12 PM - 3 PM' }
+    ],
+};
 
 const TeacherList = () => {
-  return (
-    <div className="teacher--list">
-        <div className="list--header">
-            <h2>Teachers</h2>
-            <select>
-                <option value="english">English</option>
-                <option value="bulgarian">Bulgarian</option>
-            </select>
-        </div>
-        <div className="list--container"> 
-            {teachers.map((teacher) => (
-                <div key={teacher.id} className="list"> {/* Уникален key пропс */}
+    return (
+        <div className="teacher--list">
+            <div className="list--header">
+                <h2>Your Classes</h2>
+            </div>
+            <div className="list--container">
+                <div className="list">
                     <div className="teacher--detail">
-                        <img src={teacher.image} alt={teacher.name} />
-                        <h2>{teacher.name}</h2>
+                        <img src={currentTeacher.image} alt={currentTeacher.name} />
+                        <div>
+                            <h2>{currentTeacher.name}</h2>
+                        </div>
                     </div>
-                    <span className='teacher--duration'>{teacher.duration}</span>
-                    <span className='teacher--cost'>{teacher.cost}</span>
-                    <span className="teacher--todo">:</span>
+                    <div className="schedule-container">
+                        {currentTeacher.classes.map((item, index) => (
+                            <div key={index} className="class-schedule">
+                                <h3>{item.className}</h3>
+                                <span>{item.time}</span>
+                            </div>
+                        ))}
+                    </div>
                 </div>
-            ))}
+            </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default TeacherList;
