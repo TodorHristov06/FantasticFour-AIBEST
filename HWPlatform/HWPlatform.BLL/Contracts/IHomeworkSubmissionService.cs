56b5698561be94c6d0eb5b20366521da3268bb88
Simeon Boev <85336777+SABoev20@users.noreply.github.com>
2024-07-24 18:50:47 +0300

    Create HomeworkSubmissionService

---
+++ HWPlatform/HWPlatform.BLL/Contracts/IHomeworkSubmissionService.cs
@@ -0,0 +1,9 @@
+﻿using HWPlatform.DAL.Models;
+
+namespace HWPlatform.BLL.Contracts;
+
+public interface IHomeworkSubmissionService
+{
+    Task SubmitHomeworkAsync(HomeworkSubmission homeworkSubmission);
+    Task<HomeworkSubmission> GetSubmissionByAssignmentStudentEmailAsync(string email);
+}
